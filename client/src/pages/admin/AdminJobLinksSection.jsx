eac0f5a9e978cd078f1c95e17c0aaff4db00d18e
Tony Kuriakose <t4tonykuriakose@gmail.com>
2026-07-26 22:29:34 +0530

    done

--- client/src/pages/admin/AdminJobLinksSection.jsx
+++ client/src/pages/admin/AdminJobLinksSection.jsx
@@ -37,7 +37,7 @@ export default function AdminJobLinksSection() {
   };
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchJobLinks();
   }, []);
 
