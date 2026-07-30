afdf43f6a1620c953780250e625917479f60848e
Tony Kuriakose <t4tonykuriakose@gmail.com>
2026-07-30 20:46:54 +0530

    fix server startup crash on Cloud Run when FIREBASE_STORAGE_BUCKET is undefined

--- server/utils/firebase.js
+++ server/utils/firebase.js
@@ -10,7 +10,13 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
 
 const storage = new Storage(storageOptions);
 const bucketName = process.env.FIREBASE_STORAGE_BUCKET;
-const bucket = storage.bucket(bucketName);
+
+const getBucket = () => {
+  if (!bucketName) {
+    throw new Error('FIREBASE_STORAGE_BUCKET environment variable is not defined.');
+  }
+  return storage.bucket(bucketName);
+};
 
 /**
  * Uploads a file buffer to Firebase Storage and returns its public URL.
@@ -24,6 +30,7 @@ const uploadToFirebase = async (buffer, mimeType, filename) => {
     throw new Error('FIREBASE_STORAGE_BUCKET environment variable is not defined.');
   }
 
+  const bucket = getBucket();
   const file = bucket.file(filename);
   await file.save(buffer, {
     metadata: {
@@ -53,6 +60,7 @@ const deleteFromFirebase = async (fileUrl) => {
     const prefix = `https://storage.googleapis.com/${bucketName}/`;
     if (fileUrl.startsWith(prefix)) {
       const filename = fileUrl.replace(prefix, '');
+      const bucket = getBucket();
       const file = bucket.file(filename);
       await file.delete();
       console.log(`Deleted file from Firebase Storage: ${filename}`);
@@ -65,5 +73,5 @@ const deleteFromFirebase = async (fileUrl) => {
 module.exports = {
   uploadToFirebase,
   deleteFromFirebase,
-  bucket,
+  getBucket,
 };
